6cc108e34760f3b6dd84073dd57e8ae8254e2707
xiaolongcode <450313910@qq.com>
2020-10-27 10:23:39 +0800

    添加一个web项目

---
+++ Dragon.Web/Dragon.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dragon.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
